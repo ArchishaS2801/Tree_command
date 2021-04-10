8990dbd6f5d7e8776e3d6476de74b0089179aa17
Archisha Shukla <archishashukla@gmail.com>
2021-04-10 19:04:13 +0530

    Added flags

--- main.c
+++ main.c
@@ -4,19 +4,43 @@
 #include<stdlib.h>
 #include<string.h>
 #include "tree.h"
-int main()
+int main(int argc,char *argv[])
 {
+    if(argc==1) {
+
     node *start=create_node(".");
 	start->isdir=1;
     start->nextDirectory = create_tree(getcwd(0,0));
 	print_tree(start);
+
+    }
+    
 	char name[100000];
-	printf("Enter path to list files: ");
-    scanf("%s", name);
-    print_specified_path(name);
-    get_username(name);
-    printf("\n");
-    getFileCreationTime(name);
+
+    if(argc >= 2 && strcmp(argv[1],"-f") == 0){
+
+	    printf("Enter path to list files: ");
+        scanf("%s", name);
+        print_specified_path(name);
+
+
+    }
+
+    if(argc >= 2 && strcmp(argv[1],"-u") == 0){
+
+          printf("Enter path to list files: ");
+          scanf("%s", name);
+          get_username(name);
+          printf("\n");
+    }
+
+    if(argc >= 2 && strcmp(argv[1],"-t") == 0){
+
+          getFileCreationTime(name);
+
+    }
     return 0;
+
+  
 
 }

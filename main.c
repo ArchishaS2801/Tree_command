eb5796da2d10d37ab070715ddbab95c620ef2943
Archisha Shukla <archishashukla@gmail.com>
2021-04-10 20:39:50 +0530

    Adding few more flags

--- main.c
+++ main.c
@@ -38,6 +38,7 @@ int main(int argc,char *argv[])
           getFileCreationTime(name);
 
     }
+
     if(argc >= 2 && strcmp(argv[1],"-Ft") == 0){
           printf("Enter path to list files: ");
           scanf("%s", name);
@@ -45,6 +46,33 @@ int main(int argc,char *argv[])
           
 
     }
+
+    if(argc >=2 && strcmp(argv[1],"-help") == 0){
+
+        printf("\nThis is the help section: \n");
+        printf("\nWithout flags it prints all the files/folders in current directory\n");
+        printf("\nThe various flags are : \n");
+        printf("\n-f : prints all files in given path recursively\n");
+        printf("\n-Ft : all the files and folders in the path specified along with last modified date and time \n");
+        printf("\n-t : prints the last modified of the current path \n");
+        printf("\n-v : prints the version of the tree\n");
+        printf("\n-help : prints the help section\n\n");
+
+
+    }
+
+    if(argc >= 2 && strcmp(argv[1],"-v") == 0){
+
+          printf("\ntree 1.12.1 \n");
+          printf("\nCOPYRIGHT 2021 -----ARCHISHA SHUKLA \n");
+          printf("\nTree is a free software with no warranty\n\n");
+          
+
+    }
+
+
+
+
 
     return 0;
 
